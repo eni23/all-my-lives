d929a9b1cb0ecf019fb3e5d34d928e6d0474e96e
Cyrill von Wattenwyl <eni@e23.ch>
2015-08-16 16:11:32 +0200

    setup stuff

--- pinode/tools/setup-raspi.sh
+++ pinode/tools/setup-raspi.sh
@@ -8,15 +8,15 @@ AML_LOCATION="/opt/all-my-lives"
 # update system and install deps
 apt-get update
 apt-get upgrade -y
-apt-get install vim mplayer daemontools daemontools-run -y
+apt-get install vim mplayer daemontools daemontools-run git -y
+update-rc.d dhcpd disable
 
 # nodejs & npm
 sudo dpkg -i $AML_LOCATION/pinode/tools/node_latest_armhf.deb
 curl -L https://npmjs.com/install.sh | sh
 
-# install socket.io globally, since it takes long time to rebuild on every deploy
-# npm install -g socket.io
-tar xfvz $AML_LOCATION/pinode/tools/socket.io-armhf.tar.gz
+# install precompiled socket.io
+tar xfvz $AML_LOCATION/pinode/tools/socket.io-armhf.tar.gz -C /
 
 # install aml
 cd $AML_LOCATION/pinode
@@ -27,6 +27,10 @@ mkdir /etc/service/aml-pinode
 cp $AML_LOCATION/pinode/bin/run /etc/service/aml-pinode/run
 chmod a+x /etc/service/aml-pinode/run
 chmod -R 755 /etc/service/aml-pinode
+
+mkdir /opt/media
+chmod 755 /opt/media
+cp $AML_LOCATION/pinode/test-media/* /opt/media/
 
 # better bashrc
 wget -O /etc/profile.d/adsy-bashrc.sh http://data.e23.ch/bashrc/adsy-bashrc.sh
